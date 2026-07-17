628cd7303952c7e2f159387a9551b338bff4dada
Dana Khaled Abu El-aish <45danakhaled123@gmail.com>
2026-07-17 18:16:20 +0300

    Initial commit: first C# console project setup for learning syntax

---
+++ Couse14_LearningCSharpSyntax/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Couse14_LearningCSharpSyntax
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World");
+            Console.ReadKey();
+        }
+    }
+}
