5e2ccf92428c2e9459092193c0d0559cdc3dc7b9
Dana Khaled Abu El-aish <45danakhaled123@gmail.com>
2026-07-17 20:54:07 +0300

    Lesson 1 : About C# langauge (WriteLine)

---
+++ Course14/Program.cs
@@ -0,0 +1,21 @@
+﻿// ****************************************************** 1 *******************************************************
+// ************************************************** WirteLine *******************************************************
+using System;
+
+
+namespace Course14
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("My name is Dana Abu Elaish");
+            Console.WriteLine("I'm Learning C#");
+            Console.WriteLine("It is an easy language to learn :-)");
+            Console.WriteLine("The sum of 10 + 20 = " + (10 + 20));
+
+            Console.ReadKey();
+        }
+    }
+}
