f476ec8dbf17ba0f33e6e732e2624a7a85da87ae
Dana Khaled Abu El-aish <45danakhaled123@gmail.com>
2026-07-17 21:12:04 +0300

    Lesson 2 : I learned the difference between Write and WriteLine

--- Course14/Program.cs
+++ Course14/Program.cs
@@ -1,5 +1,6 @@
 ﻿// ****************************************************** 1 *******************************************************
 // ************************************************** WirteLine *******************************************************
+/*
 using System;
 
 
@@ -14,6 +15,52 @@ namespace Course14
             Console.WriteLine("I'm Learning C#");
             Console.WriteLine("It is an easy language to learn :-)");
             Console.WriteLine("The sum of 10 + 20 = " + (10 + 20));
+
+            Console.ReadKey();
+        }
+    }
+}
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// ****************************************************** 2 *******************************************************
+// ************************************************** Wirte *******************************************************
+
+using System;
+
+
+namespace Course14
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("My name is Dana Abu Elaish");
+            Console.Write("I'm Learning C#");
+            Console.Write("It is an easy language to learn :-)");
+            Console.Write("The sum of 10 + 20 = " + (10 + 20));
 
             Console.ReadKey();
         }
