ad647c82a8002706b3ba7fe0c07aa8a53a1633c8
Dana Khaled Abu El-aish <45danakhaled123@gmail.com>
2026-07-17 20:18:48 +0300

    Add C++ to C# conversion activities project --Factorial

---
+++ ConvertActivitesFromCPPToCSharp/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace ConvertActivitesFromCPPToCSharp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.Write("Enter number: ");
+            int number = int.Parse(Console.ReadLine());
+            int fact = 1;
+            for(int i = 2; i<= number; i++)
+            {                
+                    fact = fact * i;
+            }
+            Console.Write("Factorial for " + number + " = " + fact);
+        }
+    }
+}
