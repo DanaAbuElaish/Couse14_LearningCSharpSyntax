0d7e2cfb71ccfe0d6aa9aee1e51ebaf0ea5bf9af
Dana Khaled Abu El-aish <45danakhaled123@gmail.com>
2026-07-18 13:20:11 +0300

    I deleted the last two commits because I forgot to add a commit.ه

--- Course14/Program.cs
+++ Course14/Program.cs
@@ -90,7 +90,7 @@ namespace Course14
 
 // ****************************************************** 3 *******************************************************
 // **************************************************   Formatted String *******************************************************
-/*
+
 using System;
 
 
@@ -107,120 +107,5 @@ namespace Course14
         }
     }
 }
-*/
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// ****************************************************** 4 *******************************************************
-// ************************************************** Escape Characters *******************************************************
-/*
-using System;
-
-
-namespace Course14
-{
-    internal class Program
-    {
-        static void Main(string[] args)
-        {
-
-            Console.WriteLine("Useful Escape Characters:\n");
-
-            //NewLine 
-            Console.WriteLine("NewLine:");
-            Console.WriteLine("Welcome to \n Programming Advices\n");
-
-            //Tab
-            Console.WriteLine("Tab: ");
-            Console.WriteLine("Welcome to \tProgramming Advices\n");
-
-            //Backspace
-            Console.WriteLine("Backspace:");
-            Console.WriteLine("Welcome to \bProgramming Advices\n");
-
-            //Single Quote
-            Console.WriteLine("Single Quate : ");
-            Console.WriteLine("Welcome to \'Programming  Advices\n");
-
-            //Double Quote
-            Console.WriteLine("Double Quate : ");
-            Console.WriteLine("Welcome to \\Programming  Advices\n");
-
-            //Alert
-            Console.WriteLine("Alert : ");
-            Console.WriteLine("\a");
-
-            Console.ReadKey();
-
-        }
-    }
-}
-*/
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// ****************************************************** 5 *******************************************************
-// *************************************** Single Line/Multiple Lines Comments *******************************************************
-
-using System;
-
-
-namespace Course14
-{
-    internal class Program
-    {
-        static void Main(string[] args)
-        {
-            //This is a single line comment
-            Console.WriteLine("My name is Dana Abu Elaish");
-
-            /*
-             This is
-            a multiple line
-            comment.
-             */
-
-           
-
-        }
-    }
-}
