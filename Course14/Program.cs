a077faa40d2bb57d7ed79655acecf7812cedee47
Dana Khaled Abu El-aish <45danakhaled123@gmail.com>
2026-07-20 14:22:21 +0300

    Lesson 14 : Nullable Types

--- Course14/Program.cs
+++ Course14/Program.cs
@@ -611,3 +611,53 @@ namespace Course14
     }
 }
 */
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// ****************************************************** 14 *******************************************************
+// ***************************************************** Nullable Types *****************************************************
+
+using System;
+
+namespace Main
+{
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+
+            //  Nullable<int> can be assigned any value
+            //  from -2147483648 to 2147483647, or a null value.
+
+            Nullable<int> i = null;
+
+
+
+            Console.ReadKey();
+
+        }
+    }
+}
+
