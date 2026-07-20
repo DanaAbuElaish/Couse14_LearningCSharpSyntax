d8448845f4cae8e6e082ee5fb805acb6acca0505
Dana Khaled Abu El-aish <45danakhaled123@gmail.com>
2026-07-20 13:47:08 +0300

    Lesson 12 : About Default Values for datatypes

--- Course14/Program.cs
+++ Course14/Program.cs
@@ -351,7 +351,7 @@ namespace Course14
 
 // ****************************************************** 11 *******************************************************
 // ***************************************** Numbers Datatypes *******************************************
-
+/*
 using System;
 
 
@@ -466,6 +466,64 @@ namespace Course14
 
             Console.WriteLine(hex);
             Console.WriteLine(binary);
+
+            Console.ReadKey();
+
+        }
+    }
+}
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// ****************************************************** 12 *******************************************************
+// ***************************************** Default Values *******************************************
+
+using System;
+
+
+namespace Course14
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //get default value using default(type)
+            int i = default(int); // 0
+            float f = default(float);// 0
+            decimal d = default(decimal);// 0
+            bool b = default(bool);// false
+            char c = default(char);// '\0'
+
+            // C# 7.1 onwards
+            //get default value using default
+            int i2 = default; // 0
+            float f2 = default;// 0
+            decimal d2 = default;// 0
+            bool b2 = default;// false
+            char c2 = default;// '\0'
+
+
 
             Console.ReadKey();
 
