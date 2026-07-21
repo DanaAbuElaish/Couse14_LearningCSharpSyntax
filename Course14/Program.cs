366908ff86f3bef2c40a26ae309e229dccebd40e
Dana Khaled Abu El-aish <45danakhaled123@gmail.com>
2026-07-21 19:00:34 +0300

    Lesson 18 : Get Current Datetime

--- Course14/Program.cs
+++ Course14/Program.cs
@@ -852,3 +852,47 @@ namespace Main
     }
 }
 */
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// ****************************************************** 18 ******************************************************
+// ********************************************** Get Current Datetime *************************************************
+
+using System;
+
+namespace Main
+{
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+            //assigns default value 01/01/0001 00:00:00
+            DateTime dt1 = new DateTime();
+
+            dt1 = DateTime.Now;
+            Console.WriteLine(dt1);
+            Console.ReadKey();
+        }
+
+    }
+}
