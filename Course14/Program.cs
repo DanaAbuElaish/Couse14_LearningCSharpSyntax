e18a687dcd476fdaec8fab65983e3c3f94bc8912
Dana Khaled Abu El-aish <45danakhaled123@gmail.com>
2026-07-22 21:42:24 +0300

    Lesson 45 : while loop

--- Course14/Program.cs
+++ Course14/Program.cs
@@ -2044,3 +2044,50 @@ namespace Main
     }
 }
 */
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// *********************************************************** 45 *************************************************************
+// ***************************************************** while loop ***********************************
+
+using System;
+
+
+namespace Main
+{
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+
+            int i = 1;
+            while (i <= 5)
+            {
+                Console.WriteLine("C# while Loop: Iteration {0}", i);
+                i++;
+            }
+
+            Console.ReadKey();
+
+        }
+    }
+}
