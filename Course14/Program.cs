9ba06151e8e0f4a9e41cceb29f8198b79b4953e3
Dana Khaled Abu El-aish <45danakhaled123@gmail.com>
2026-07-22 20:22:55 +0300

    Lesson 39 : Ternary Operators

--- Course14/Program.cs
+++ Course14/Program.cs
@@ -1684,3 +1684,51 @@ namespace Main
     }
 }
 */
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// *********************************************************** 39 *************************************************************
+// ***************************************************** Ternary Operators ***********************************
+
+using System;
+
+namespace Main
+{
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+
+
+            int number = 12;
+            string result;
+
+            result = (number % 2 == 0) ? "Even Number" : "Odd Number";
+            Console.WriteLine("{0} is {1}", number, result);
+
+
+            Console.ReadKey();
+        }
+    }
+}
