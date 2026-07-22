be30624add762014ab0464ff2e32053b791b1048
Dana Khaled Abu El-aish <45danakhaled123@gmail.com>
2026-07-22 13:08:46 +0300

    Lesson 21 : Time Span

--- Course14/Program.cs
+++ Course14/Program.cs
@@ -993,3 +993,55 @@ namespace Main
     }
 }
 */
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// *********************************************************** 21 ******************************************************
+// ******************************************************* Time Span ***************************************************
+
+using System;
+
+namespace Main
+{
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+
+            DateTime dt = new DateTime(2026, 7, 22);
+
+            // Hours, Minutes, Seconds
+            TimeSpan ts = new TimeSpan(49, 25, 34);
+            Console.WriteLine(ts);
+            Console.WriteLine(ts.Days);
+            Console.WriteLine(ts.Hours);
+            Console.WriteLine(ts.Minutes);
+            Console.WriteLine(ts.Seconds);
+
+            //this will add time span to the date.
+            DateTime newDate = dt.Add(ts);
+
+            Console.WriteLine(newDate);
+
+            Console.ReadKey();
+        }
+    }
+}
