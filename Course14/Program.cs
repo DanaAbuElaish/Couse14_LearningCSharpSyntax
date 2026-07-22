554026600f78e0670782e543e471aedd6b6a2b9b
Dana Khaled Abu El-aish <45danakhaled123@gmail.com>
2026-07-22 21:48:40 +0300

    Lesson 47 : break Statement

--- Course14/Program.cs
+++ Course14/Program.cs
@@ -2137,3 +2137,60 @@ namespace Main
     }
 }
 */
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// *********************************************************** 47 *************************************************************
+// ***************************************************** break Statement ******************************************************
+
+using System;
+
+
+namespace Main
+{
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+
+            int i = 1;
+            do
+            {
+                Console.WriteLine("C# do-while Loop: Iteration {0}", i);
+
+                if (i == 3)
+                {
+                    break;
+                }
+                i++;
+
+            } while (i <= 5);
+
+
+            Console.ReadKey();
+
+        }
+    }
+}
