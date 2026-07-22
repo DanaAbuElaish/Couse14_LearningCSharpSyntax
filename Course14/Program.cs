86ddfec3a40a89c5fe08e0c140919d945eeed027
Dana Khaled Abu El-aish <45danakhaled123@gmail.com>
2026-07-22 14:45:56 +0300

    Lesson 24 : Convert String to DateTime

--- Course14/Program.cs
+++ Course14/Program.cs
@@ -1143,3 +1143,68 @@ namespace Main
     }
 }
 */
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// *********************************************************** 24 *************************************************************
+// *************************************************** Convert String to DateTime *************************************
+
+using System;
+using System.Runtime.CompilerServices;
+
+
+namespace Main
+{
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+
+            var str = "6/12/2023";
+            DateTime dt;
+
+            var isValidDate = DateTime.TryParse(str,out dt);
+
+            if (isValidDate)
+            {
+                Console.WriteLine(dt);
+            }
+            else
+            {
+                Console.WriteLine($"{str} is not a valid date string");
+
+            }
+
+
+            //invalid string date
+            var str2 = "6/65/2023";
+            DateTime dt2;
+
+            var isValidDate2 = DateTime.TryParse(str2, out dt2);
+
+            if (isValidDate2)
+                Console.WriteLine(dt2);
+            else
+                Console.WriteLine($"{str2} is not a valid date string");
+
+            Console.ReadKey();
+        }
+    }
+}
