2cc7a4e78dcb0e0ef559f9ea87e85082ecbfc799
Dana Khaled Abu El-aish <45danakhaled123@gmail.com>
2026-07-22 21:39:02 +0300

    Lesson 44 :  for loop

--- Course14/Program.cs
+++ Course14/Program.cs
@@ -1975,3 +1975,71 @@ namespace Main
     }
 }
 */
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// *********************************************************** 44 *************************************************************
+// ***************************************************** for loop ***********************************
+
+using System;
+
+
+namespace Main
+{
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Forward Loop : ");
+
+            //Forward Loop
+            for (int i = 1; i <= 10; i++)
+            {
+                Console.WriteLine(i);
+            }
+
+
+
+            Console.WriteLine("Backward Loop : ");
+
+            //Back Loop
+            for (int i = 10; i >= 1; i--)
+            {
+                Console.WriteLine(i);
+            }
+
+            Console.WriteLine("Nested Loops : ");
+            //Forward Loop
+            for (int i = 1; i <= 10; i++)
+            {
+                for (int j = 0; j < 10; j++)
+                {
+                    Console.WriteLine("i = {0} and j = {1}", i, j);
+                }
+            }
+
+            Console.ReadKey();
+
+        }
+    }
+}
