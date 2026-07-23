4b0ce5a86185cdcf271ef5c9358cd2f9bd3cfa4c
Dana Khaled Abu El-aish <45danakhaled123@gmail.com>
2026-07-23 12:49:14 +0300

    Lesson 54 : 2 Dimensional Array

--- Course14/Program.cs
+++ Course14/Program.cs
@@ -2301,3 +2301,50 @@ namespace Main
     }
 }
 */
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// *********************************************************** 54 *************************************************************
+// ************************************************* 2 Dimensional Array ******************************************************
+
+using System;
+
+
+namespace Main
+{
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+
+            //initializing 2D array
+            int[,] numbers = { { 12, 13 }, { 55, 77 } };
+
+            // access first element from the first row
+            Console.WriteLine("Elements at index [0,0] : " + numbers[0, 0]);
+
+            // access first element from second row
+            Console.WriteLine("Element at index [1,0] : " + numbers[1, 0]);
+
+            Console.ReadKey();
+
+        }
+    }
+}
